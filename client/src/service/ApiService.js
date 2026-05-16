9e35f6919970f92e9869a7534325ca2b36226ad7
Vladislav Shorodok <vlad.shorodok14@gmail.com>
2026-05-16 12:12:12 +0300

    add netlify

--- client/src/service/ApiService.js
+++ client/src/service/ApiService.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export default class ApiService {
-    static BASE_URL = "http://localhost:2424"
+    static BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:2424"
 
     static getHeader() {
         const token = localStorage.getItem("token");
